30e95f6a1b9e5154997a26351353dc13bbe9cd24
tom <5075314+tombloor@users.noreply.github.com>
2020-01-23 14:13:55 -0600

    post to comment.md

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+from datetime import datetime
+from flask import Flask, abort, request
 from app import app
 
 
@@ -6,9 +8,32 @@ from app import app
 def health_check():
     return f"I'm still alive. Debug Mode: {str(app.config['DEBUG'])}"
 
-@app.route('/comment')
+@app.route('/comment', methods=['POST'])
 def new_comment():
-    ret = "New comment, config should say where it's going n how it's created."
-    ret += f" Comment will be saved at {app.config['COMMENT_PATH']}"
-    return ret
+    params = {
+        'text': request.values.get('text', ''),
+        'name': request.values.get('name', 'Anonymous'),
+        'image': request.values.get('image', '')
+    }
+
+    if request.method == 'POST':
+        text = params['text'].strip()
+        if text == '':
+            abort(400)
+
+        lines = [
+            '---',
+            f'date: {datetime.now().strftime("%Y-%m-%d %H:%M:%S %z")}',
+            f'name: {params["name"].strip()}',
+            f'image: {params["image"]}',
+            '---',
+            text,
+            ''  # This gives us the blank line at EOF
+        ]
+
+        comment = os.linesep.join(lines)
+
+        return comment
+    else:
+        abort(405)
     
