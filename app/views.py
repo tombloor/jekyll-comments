be339052f9320bef3b57b0cb8b97279a70e76b64
tom <5075314+tombloor@users.noreply.github.com>
2020-01-23 11:52:56 -0600

    added .env config

--- app/views.py
+++ app/views.py
@@ -4,9 +4,11 @@ from app import app
 
 @app.route('/')
 def health_check():
-    return "I'm still alive"
+    return f"I'm still alive. Debug Mode: {str(app.config['DEBUG'])}"
 
 @app.route('/comment')
 def new_comment():
-    return "New comment, config should say where it's going n how it's created"
+    ret = "New comment, config should say where it's going n how it's created."
+    ret += f" Comment will be saved at {app.config['COMMENT_PATH']}"
+    return ret
     
--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-# Should be loading this from .env file
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Enable Flask's debugging features. Should be False in production
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-COMMENT_MODE='file'
-#COMMENT_MODE='git'
+COMMENT_MODE=os.getenv('COMMENT_MODE')
 
 #abs path when in file mode, path from repo root when in git mode
-#COMMENT_FOLDER=
+COMMENT_PATH=os.getenv('COMMENT_PATH')
 
 #COMMENT_GIT_REPOSITORY=
 #COMMENT_GIT_USERNAME=
