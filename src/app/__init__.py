5c26d69e4d3f29d5390977fba999a3add07d4b56
Tom Bloor <5075314+tombloor@users.noreply.github.com>
2020-02-06 21:09:04 -0600

    remove cors and restructure
    Restructure ready for dockerization, after I add git support.
    Removed CORS as it should be handled by the web proxy

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
-CORS(app)
 
 # Load the views
 from app import views
--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = os.getenv('DEBUG', False)
+FLASK_RUN_PORT = os.getenv('FLASK_RUN_PORT')
 
 COMMENT_MODE=os.getenv('COMMENT_MODE')
 
@@ -12,8 +13,8 @@ COMMENT_MODE=os.getenv('COMMENT_MODE')
 COMMENT_PATH=os.getenv('COMMENT_PATH')
 
 #COMMENT_GIT_REPOSITORY=
-#COMMENT_GIT_USERNAME=
-#COMMENT_GIT_PASSWORD=
-#COMMENT_GIT_BRANCH_PREFIX=
+COMMENT_GIT_REPOSITORY=os.getenv('COMMENT_GIT_REPOSITORY')
+COMMENT_GIT_TOKEN=os.getenv('COMMENT_GIT_TOKEN')
+COMMENT_GIT_FOLDER=os.getenv('COMMENT_GIT_FOLDER')
 
 TZ=os.getenv('TZ')
