6741654c0a34dd1598dbd101505cb020ad2e28b4
Tom Bloor <5075314+tombloor@users.noreply.github.com>
2020-02-06 21:53:20 -0600

    git support

--- src/app/views.py
+++ src/app/views.py
@@ -7,6 +7,7 @@ import requests
 
 from datetime import datetime
 from flask import Flask, abort, request, Response
+from github import Github
 from app import app
 
 
@@ -46,19 +47,19 @@ def new_comment():
             ''  # This gives us the blank line at EOF
         ]
 
-        comment = os.linesep.join(lines)
+        perma_strip = perma.rstrip('/')
+        slug = perma_strip[perma_strip.rfind('/') + 1:]
+        d = date.strftime("%Y%m%d_%H%M%S")
+        filename = f'{d}_{slug}.md'
 
         if app.config['COMMENT_MODE'] == 'local':
-            perma_strip = perma.rstrip('/')
-            slug = perma_strip[perma_strip.rfind('/') + 1:]
-            d = date.strftime("%Y%m%d_%H%M%S")
-            filename = f'{d}_{slug}.md'
+            comment = os.linesep.join(lines)
             write_comment_local(app.config['COMMENT_PATH'], filename, comment)
             return Response(status=200)
         elif app.config['COMMENT_MODE'] == 'git':
-            # https://developer.github.com/v3/repos/contents/#create-or-update-a-file
-            # https://developer.github.com/v3/pulls/#create-a-pull-request
-            pass
+            comment = '\n'.join(lines)
+            write_comment_git(app.config['COMMENT_PATH'], filename, comment)
+            return Response(status=200)
         else:
             return comment
     else:
@@ -86,3 +87,26 @@ def write_comment_local(path, filename, comment):
 
     with p.open('w') as f:
         f.write(comment)
+
+def write_comment_git(path, filename, comment):
+    g = Github(app.config['COMMENT_GIT_TOKEN'])
+
+    repo = g.get_repo(app.config['COMMENT_GIT_REPOSITORY'])
+
+    default_branch = repo.get_branch(repo.default_branch).commit.sha
+    base = None
+    try:
+        base = repo.get_branch(app.config['COMMENT_GIT_FOLDER'] + '/moderated')
+    except Exception as err:
+        repo.create_git_ref(app.config['COMMENT_GIT_FOLDER'] + '/moderated', default_branch)
+        base = repo.get_branch(app.config['COMMENT_GIT_FOLDER'] + '/moderated')
+
+    new_branchname = filename.replace('.md', '')
+    branch = repo.create_git_ref(f"refs/heads/{app.config['COMMENT_GIT_FOLDER']}/{new_branchname}", default_branch)
+    
+    p = pathlib.Path(path) / filename
+    commit_msg = f'New comment: {new_branchname}'
+    repo.create_file(str(p), commit_msg, comment, branch=branch.ref)
+
+    pr = repo.create_pull(title=commit_msg, body=f"```{comment}```", head=branch.ref, base=base.name)
+    return pr
